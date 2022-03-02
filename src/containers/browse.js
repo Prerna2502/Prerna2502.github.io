3247fd5ecb7a909bfca4a950ded8e7f3a54c9585
prerna.agarwal <prerna.agarwal@globallogic.com>
2022-03-03 01:56:57 +0530

    videoadded

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,19 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import { getAuth } from "firebase/auth";
-import {Loading, Header, Card} from '../compoments';
+import {Loading, Header, Card, Player} from '../compoments';
+import {FooterContainer} from './footer';
 import * as ROUTES from '../constants/routes';
 import logo from "../logo.png";
 
 
 export function BrowseContainer({slides}){
-    // const [category, setCategory] = useState('series');
+    const [category, setCategory] = useState('series');
     const [ searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const {firebase} = useContext(FirebaseContext);
     const user = getAuth(firebase).currentUser || {};
-    // const [slideRows, setSlideRows] = useState([]);
+    const [slideRows, setSlideRows] = useState([]);
 
     useEffect(() => {
         setTimeout(() => {
@@ -22,9 +23,9 @@ export function BrowseContainer({slides}){
         }, 3000);
     }, [profile.displayName]);
 
-    // useEffect(() => {
-    //     setSlideRows(slides[category]);
-    // },[]);
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    },[slides, category]);
 
     return profile.displayName ? (
 
@@ -38,8 +39,8 @@ export function BrowseContainer({slides}){
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src={logo} alt="IndianMovies" />
-                        <Header.TextLink>Series</Header.TextLink>
-                        <Header.TextLink>Films</Header.TextLink>
+                        <Header.TextLink active={category === 'series' ? 'true': 'false'} onClick={() => setCategory('series')}>Series</Header.TextLink>
+                        <Header.TextLink active={category === 'films' ? 'true': 'false'} onClick={() => setCategory('films')}>Films</Header.TextLink>
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
@@ -68,8 +69,30 @@ export function BrowseContainer({slides}){
                 </Header.Feature>
             </Header>
             <Card.Group>
-
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <Player>
+                                <Player.Button/>
+                                <Player.Video src="/videos/bunny.mp4" />
+                            </Player>
+                        </Card.Feature>
+                    </Card>
+                ))};
             </Card.Group>
+            <FooterContainer/>
             </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile}/>
